f2c6e10ab24fe2af282021b6dea5482051539d37
JoSe1411 <joysen.3002@gmail.com>
2025-12-01 16:54:05 +0530

    Remove documentation comments from application class
    Removed application documentation comments from BajajQualifierApplication.java.

--- src/main/java/com/bajaj/qualifier/BajajQualifierApplication.java
+++ src/main/java/com/bajaj/qualifier/BajajQualifierApplication.java
@@ -3,16 +3,7 @@ package com.bajaj.qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Bajaj Finserv Health Qualifier 1 - JAVA
- * 
- * This application automatically:
- * 1. Sends POST request to generate webhook on startup
- * 2. Extracts webhook URL and accessToken
- * 3. Submits SQL query solution to the webhook
- * 
- * @author Bajaj Qualifier Submission
- */
+
 @SpringBootApplication
 public class BajajQualifierApplication {
 
