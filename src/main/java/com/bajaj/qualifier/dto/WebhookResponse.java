81c0bf36eeb58607d6dca926d88e1af5626caad0
JoSe1411 <joysen.3002@gmail.com>
2025-12-01 16:52:09 +0530

    Update WebhookResponse.java

--- src/main/java/com/bajaj/qualifier/dto/WebhookResponse.java
+++ src/main/java/com/bajaj/qualifier/dto/WebhookResponse.java
@@ -6,10 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Response DTO from generateWebhook API.
- * Contains webhook URL and access token for authentication.
- */
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
