a7c99eb7f19bf304d549c77fac931ed43cbfb969
JoSe1411 <joysen.3002@gmail.com>
2025-12-01 16:51:58 +0530

    Clean up comments in WebhookRequest.java
    Removed unnecessary comments from WebhookRequest.java

--- src/main/java/com/bajaj/qualifier/dto/WebhookRequest.java
+++ src/main/java/com/bajaj/qualifier/dto/WebhookRequest.java
@@ -6,10 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Request DTO for generating webhook.
- * Contains user details required for the POST request.
- */
+
 @Data
 @Builder
 @NoArgsConstructor
